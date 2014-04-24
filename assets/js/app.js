03d0d03799a4eb9cf8cae93be3c99470292c3872
NKjoep <nkjoep@gmail.com>
2014-04-24 20:18:40 +0200

    new blog post: how to learn css layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,22 +16,25 @@ jQuery(function() {
 	//contact_list.mouseenter( function() { scontacts.slideDown(); } );
 	//contact_list.mouseleave( function() { scontacts.slideUp(); } );
 
+	if (!(/^localhost/.test(window.location.host))) {
 
-	if (!window.jXHR){
-			var jxhr = document.createElement('script');
-			jxhr.type = 'text/javascript';
-			jxhr.src = window.BASE_PATH+'/assets/resources/jxhr.js';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(jxhr, s);
+		if (!window.jXHR){
+				var jxhr = document.createElement('script');
+				jxhr.type = 'text/javascript';
+				jxhr.src = window.BASE_PATH+'/assets/resources/jxhr.js';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(jxhr, s);
+		}
+
+		github.showRepos({
+		        user: 'nkjoep',
+		       count: 0,
+	  	skip_forks: true,
+		      target: '#gh-repos',
+		   itemClass: '',
+		descriptions: false
+		});
 	}
 
-	github.showRepos({
-	        user: 'nkjoep',
-	       count: 0,
-  	skip_forks: true,
-	      target: '#gh-repos',
-	   itemClass: '',
-	descriptions: false
-	});
 
 }); //domready
